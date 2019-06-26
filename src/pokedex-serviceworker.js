543712be6eb4e31d67e6cfd7b2f8427afd858a33
lieven.vetsuypens@axxes.com <lieven.vetsuypens@axxes.com>
2019-06-26 12:44:14 +0200

    pokemon correction

--- src/pokedex-serviceworker.js
+++ src/pokedex-serviceworker.js
@@ -4,7 +4,3 @@ self.addEventListener("install", function(event) {
 self.addEventListener("active", function(event){
     console.log("active event")
 })
-
-self.addEventListener("fetch", function(re){
-    return event.
-})
